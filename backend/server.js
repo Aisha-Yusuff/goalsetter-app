25c928760d3323b50d3c32a680f0781872f26ffd
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-31 09:46:37 +0100

    Create express server
    Initialise express using app, create port with .env file, create first route /api/goals and set status of get request

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 4000; // use port from .env or 5000 if not working
+
+// initialise express
+const app = express();
+
+app.get("/api/goals", (req, res) =>
+  res.status(200).res.json({ message: "Get goals" })
+);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
