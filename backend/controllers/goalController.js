67a0335fb253036511255defd20a03c37f9bf6a5
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-31 11:01:09 +0100

    Update controller functions to be asynchronous
    Install express-async-handler to handle async code and errors

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,34 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get all goals
 // @route GET /api/goals
 // @acess Private - need authentication
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
 // @desc Set new goal
 // @route POST /api/goals
 // @acess Private - need authentication
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("please add a text field");
   }
   res.status(200).json({ message: "Set goals" });
-};
+});
 
 // @desc  Update goal
 // @route PUT /api/goals
 // @acess Private - need authentication
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 // @desc  Delete goal
 // @route DELETE /api/goals
 // @acess Private - need authentication
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
